57aea4538e5261863451dde30681238859d56ab0
Rudy <rudraksh.monga@dulwich-suzhou.cn>
2020-11-06 23:01:50 +0800

    This is my second commit

--- myPython.py
+++ myPython.py
@@ -2,3 +2,14 @@ import sys
 print(sys.version)
 
 print("Hello World! This is my first Git/GitHub attempt. Wish me luck!")
+
+#lets do something a little more extra
+
+def factorial(x):
+	if x == 1:
+		return 1
+
+	return (x*factorial(x-1))
+
+number = factorial(5)
+print(number)
