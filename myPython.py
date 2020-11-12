19599e72034da9578fd703b8ff637725f95190e2
Rudy <rudraksh.monga@dulwich-suzhou.cn>
2020-11-12 22:57:39 +0800

    added numpy... hope this works

--- myPython.py
+++ myPython.py
@@ -1,9 +1,14 @@
 import sys
+import numpy as np
 print(sys.version)
 
 print("Hello World! This is my first Git/GitHub attempt. Wish me luck!")
 
 #lets do something a little more extra
+
+myList = [1, 2, 3, 4, 5]
+npMyList = np.array(myList)
+print (npMyList**2)
 
 def factorial(x):
 	if x == 1:
